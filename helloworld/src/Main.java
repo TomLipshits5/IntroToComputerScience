e97d25d73c714da132365084023c5826ffddb169
TomColl <tom20101995@gmail.com>
2020-12-14 17:55:08 +0200

    set operations class

--- helloworld/src/Main.java
+++ helloworld/src/Main.java
@@ -1,29 +1,24 @@
 
 public class Main {
     public static void main(String[] args) {
-        SetAsDynamicArray<Integer> set = new SetAsDynamicArray<Integer>();
-        LinkedList<String> list = new LinkedList<>();
-        set.add(1);
-        set.add(3);
-        set.add(1);
-        set.add(5);
-        list.add("tom");
-        list.add("gefen");
-        list.add("amit");
-        list.add("omer");
-        list.add("maor");
-        list.add("dvir");
+        SetAsDynamicArray<Integer> set1 = new SetAsDynamicArray<Integer>();
+        SetAsDynamicArray<Integer> set2 = new SetAsDynamicArray<Integer>();
 
-        Iterator<Integer> setIterator = set.iterator();
-        while(setIterator.hasNext()) {
-            System.out.println(setIterator.next());
-        }
-        int counter =0;
-        Iterator<String> listIterator = list.iterator();
-        while(listIterator.hasNext()){
-            System.out.println(counter + " " + listIterator.next());
-            counter++;
-        }
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        set1.add(4);
+        set2.add(2);
+        set2.add(3);
+        set2.add(4);
+        set2.add(5);
+//        System.out.println(SetOperations.subset(set1,set2));
+//        Set<Integer> inter = SetOperations.intersection(set1,set2);
+//        System.out.println(inter.toString());
+        Set<Integer> union = SetOperations.union(set1,set2);
+        System.out.println(union.toString());
+
+
 
 
 
--- helloworld/src/SetOperations.java
+++ helloworld/src/SetOperations.java
@@ -1,13 +1,30 @@
-public class SetOperations {
+public class SetOperations<T> {
     public static <T> boolean subset(Set<T> setA, Set<T> setB){
-        throw new UnsupportedOperationException();
+        Iterator<T> setAIterator = setA.iterator();
+        while(setAIterator.hasNext()){
+            if(!setB.contains(setAIterator.next())){
+                return false;
+            }
+        }
+        return true;
     }
 
     public static <T> Set<T> union(Set<T> setA, Set<T> setB){
-        throw new UnsupportedOperationException();
+        Iterator<T> setAIterator = setA.iterator();
+        while (setAIterator.hasNext()){
+            setB.add(setAIterator.next());
+        }
+        return setB;
     }
 
     public static <T> Set<T> intersection(Set<T> setA, Set<T> setB){
-        throw new UnsupportedOperationException();
+        Set<T> newSet = new SetAsDynamicArray<T>();
+        Iterator<T> setAIterator = setA.iterator();
+        while(setAIterator.hasNext()){
+            if (setB.contains(setAIterator.next())){
+                newSet.add(setAIterator.next());
+            }
+        }
+        return newSet;
     }
 }
