b7c83a2a006b0d8a823525499809af8702c8f6a7
TomColl <tom20101995@gmail.com>
2020-12-27 21:33:37 +0200

    assignment 4 final version

--- ASSIGNMENT4/src/Main.java
+++ ASSIGNMENT4/src/Main.java
@@ -5,22 +5,16 @@ public class Main {
     public static void main(String[] args){
 //        BinaryNumber num1 = new BinaryNumber('2');
 //        BinaryNumber num2 = new BinaryNumber('3');
-        BinaryNumber num3 = new BinaryNumber('0');
-        BinaryNumber num4 = new BinaryNumber("2147483647");
+        BinaryNumber num3 = new BinaryNumber("12");
+        BinaryNumber num4 = new BinaryNumber("0");
 //
 ////        BinaryNumber num5 = num1.negate();
 ////        BinaryNumber num6 = num2.negate();
 ////        BinaryNumber num7 = num3.negate();
 ////        BinaryNumber num8 = num4.negate();
 ////
-        BinaryNumber fib = new BinaryNumber(("-2147483649"));
-        Double m = 2147483647.0;
-        Double d = 2.147483647E9;
-//        System.out.println((int)Math.pow(2.0,52.0));
-//        System.out.println(num4.length());
-
-        String s = "110000000000000000000000000000000";
-        System.out.println(fib.toInt());
+        System.out.println(num3.multiply(num4));
+        System.out.println(num4.multiply(num3));
 
 
 
